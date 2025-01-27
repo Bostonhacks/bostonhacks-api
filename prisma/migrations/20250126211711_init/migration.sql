b3ac5e0d7ead22a203916c4bbf84f984cc98fbf9
declanyg <declan@youngs.asia>
2025-01-27 01:16:28 -0500

    Updated prisma schema. Will work on controllers later

--- prisma/migrations/20250126211711_init/migration.sql
+++ prisma/migrations/20250126211711_init/migration.sql
@@ -2,15 +2,6 @@
 CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
 
 -- CreateTable
-CREATE TABLE "Profile" (
-    "id" SERIAL NOT NULL,
-    "bio" TEXT,
-    "userId" INTEGER NOT NULL,
-
-    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "User" (
     "id" SERIAL NOT NULL,
     "email" TEXT NOT NULL,
@@ -24,21 +15,35 @@ CREATE TABLE "User" (
 -- CreateTable
 CREATE TABLE "Application" (
     "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "pronous" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "ethnicity" TEXT NOT NULL,
+    "gradYear" INTEGER NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
+    "school" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "educationLevel" TEXT NOT NULL,
+    "major" TEXT NOT NULL,
+    "diet" TEXT NOT NULL,
+    "shirtSize" TEXT NOT NULL,
+    "sleep" BOOLEAN NOT NULL,
+    "github" TEXT NOT NULL,
+    "linkedin" TEXT NOT NULL,
+    "portfolio" TEXT NOT NULL,
+    "whyBostonhacks" TEXT NOT NULL,
     "userId" INTEGER NOT NULL,
 
     CONSTRAINT "Application_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
-
--- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
--- AddForeignKey
-ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+-- CreateIndex
+CREATE UNIQUE INDEX "Application_phoneNumber_key" ON "Application"("phoneNumber");
 
 -- AddForeignKey
 ALTER TABLE "Application" ADD CONSTRAINT "Application_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
