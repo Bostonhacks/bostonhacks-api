9ca51596f758dd0f497a06fffed8c57bd5e72ef3
d0w <derekxu888@gmail.com>
2025-08-26 18:21:57 -0700

    add user update

--- app/routes/User.routes.js
+++ app/routes/User.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getSelf, getUser } from "../controllers/User.controller.js";
+import { getSelf, getUser, updateUser } from "../controllers/User.controller.js";
 import { verifyToken } from "../middleware/verifyToken.js";
 
 const router = express.Router();
@@ -82,6 +82,68 @@ router.get("/", verifyToken, getUser);
 router.get("/me", verifyToken, getSelf);
 
 // update user should prevent updates to certain fields
-// router.put("/:id", verifyToken, updateUser);
+/**
+ * @openapi
+ * 
+ * /user/{id}:
+ *  put:
+ *      summary: Update user information
+ *      description: Update user's firstName and lastName. Users can only update their own information.
+ *      tags: [User]
+ *      security:
+ *          - accessToken: []
+ *      parameters:
+ *          - in: cookie
+ *            name: access_token
+ *            description: Login token
+ *            required: true
+ *            schema:
+ *              type: string
+ *          - in: path
+ *            name: id
+ *            description: User ID to update
+ *            required: true
+ *            schema:
+ *              type: string
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          firstName:
+ *                              type: string
+ *                              description: User's first name
+ *                          lastName:
+ *                              type: string
+ *                              description: User's last name
+ *      responses:
+ *          200:
+ *              description: User updated successfully
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          $ref: "#/components/schemas/User"
+ *          400:
+ *              description: Bad request - missing ID parameter or invalid request data
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              message:
+ *                                  type: string
+ *                              error:
+ *                                  type: array
+ *          403:
+ *              $ref: "#/components/responses/403forbidden"
+ *          401:
+ *              $ref: "#/components/responses/401unauthorized"
+ *          500:
+ *              $ref: "#/components/responses/500internalservererror"
+ */
+router.put("/:id", verifyToken, updateUser);
 
 export default router;
